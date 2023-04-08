cf09eeed467808b856c0552d4313a7c9990c5d6e
Charlie <bandstracharlie@gmail.com>
2023-04-07 18:28:12 -0600

    Build pointer shop item

---
+++ src/components/shopItems/Pointer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Pointer = () => {
+  return (
+    <div className="shop-item" >
+        <img className="pointer-icon" src="/pointer.svg" />
+        <p className="item-label">cursor</p>
+    </div>
+  )
+}
+
+export default Pointer;
