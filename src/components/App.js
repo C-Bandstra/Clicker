bf12df38a714def29219b0bb33258cfaf94b2fdc
Charlie <bandstracharlie@gmail.com>
2023-04-07 18:28:03 -0600

    Update App with new components

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Shop from './Shop';
+import Clicker from './Clicker'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Shop />
+        <Clicker />
+      </header>
+    </div>
+  )
+}
+
+export default App;
